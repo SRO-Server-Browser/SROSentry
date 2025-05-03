cf1c92abfa82b9f9b935028c8b669346c3435eb3
Veysel KANTARCILAR <41362595+kantrveysel@users.noreply.github.com>
2025-05-03 09:00:47 +0300

    Create example_usage.sql

---
+++ sqls/example_usage.sql
@@ -0,0 +1,5 @@
+INSERT INTO Panel_Silvarya.dbo._Announcement_AI (_message, TargetCharName, TargetCharID, _private)
+VALUES
+('Medusa etkinliği başladı! Katılmayı unutmayın.', NULL, NULL, 0),
+('Roc Dağında fırtına kopuyor! Tigergirl ortaya çıktı!', NULL, NULL, 0),
+('Jangan şehrinde haftalık ticaret festivali başladı. Karavanlar yola çıktı!', NULL, NULL, 0);
